0f481b07ff92d285ad004faea0a9044ba735690f
Emil Föger <emil.foeger@gmail.com>
2026-01-09 11:48:42 +0100

    Created .NET 8 and .NET Framework 4.8 Console apps

---
+++ IL HW-ID Checker NET8/Program.cs
@@ -0,0 +1,78 @@
+﻿using IntelliLock.Licensing;
+
+namespace IL_HW_ID_Checker_NET8;
+
+internal class Program
+{
+    private static void Main(string[] _)
+    {
+        var cpuHwId = HardwareID.GetHardwareID(
+            CPU:       true,
+            HDD:       false,
+            MAC:       false,
+            Mainboard: false,
+            BIOS:      false,
+            OS:        false);
+
+        var hddHwId = HardwareID.GetHardwareID(
+            CPU:       false,
+            HDD:       true,
+            MAC:       false,
+            Mainboard: false,
+            BIOS:      false,
+            OS:        false);
+
+        var macHwId = HardwareID.GetHardwareID(
+            CPU:       false,
+            HDD:       false,
+            MAC:       true,
+            Mainboard: false,
+            BIOS:      false,
+            OS:        false);
+
+        var maiboardHwId = HardwareID.GetHardwareID(
+            CPU:       false,
+            HDD:       false,
+            MAC:       false,
+            Mainboard: true,
+            BIOS:      false,
+            OS:        false);
+
+        var biosHwId = HardwareID.GetHardwareID(
+            CPU:       false,
+            HDD:       false,
+            MAC:       false,
+            Mainboard: false,
+            BIOS:      true,
+            OS:        false);
+
+        var osHwId = HardwareID.GetHardwareID(
+            CPU:       false,
+            HDD:       false,
+            MAC:       false,
+            Mainboard: false,
+            BIOS:      false,
+            OS:        true);
+
+        var fullHwId = HardwareID.GetHardwareID(
+            CPU:       true,
+            HDD:       true,
+            MAC:       true,
+            Mainboard: true,
+            BIOS:      true,
+            OS:        true);
+
+        Console.WriteLine("=== HARDWARE IDs ===");
+        Console.WriteLine();
+        Console.WriteLine($"CPU:        {cpuHwId}");
+        Console.WriteLine($"HDD:        {hddHwId}");
+        Console.WriteLine($"MAC:        {macHwId}");
+        Console.WriteLine($"Mainboard:  {maiboardHwId}");
+        Console.WriteLine($"BIOS:       {biosHwId}");
+        Console.WriteLine($"OS:         {osHwId}");
+        Console.WriteLine($"FULL:       {fullHwId}");
+
+        Console.WriteLine("Taste drücken um Programm zu beenden");
+        Console.ReadLine();
+    }
+}
